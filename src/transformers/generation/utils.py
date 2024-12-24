641ee001ab1adb79eb0b951b1de87f3e9f5eb708
mengfeil <mengfei.li@intel.com>
2024-12-24 16:09:16 +0800

    import time

--- src/transformers/generation/utils.py
+++ src/transformers/generation/utils.py
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import copy
+import time
 import inspect
 import warnings
 from dataclasses import dataclass
